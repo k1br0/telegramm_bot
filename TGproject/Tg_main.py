7a9f29fad661bdd45f3949b9a5ffc76a25d045e6
k1br0 <81685132+k1br0@users.noreply.github.com>
2022-05-15 00:44:14 +0300

    Update Tg_main.py

--- TGproject/Tg_main.py
+++ TGproject/Tg_main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import requests
 from auth_data import token
 
@@ -6,7 +7,20 @@ def telegram_bot(token):
     bot = telebot.TeleBot(token)
     @bot.message_handler(commands=["start"])
     def start(message):
-        bot.send_message(message.chat.id , "Добрый день")
+        bot.send_message(message.chat.id ,
+                         f"<b>Добрый день, {message.from_user.first_name}</b>" ,
+                     parse_mode="html")
+        bot.send_message(message.chat.id, f"Это телеграм бот, где ты можешь следить за миром криптовылют\nВыбери что конкретно тебя интересует...",
+                         parse_mode="html")
+
+    @bot.message_handler(commands=["start"])
+    def webite(message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        curse = types.KeyboardButton("Курсы валют")
+        news = types.KeyboardButton("Новости крипто мира")
+        markup.add(curse , news)
+        bot.send_message(message.chat.id , "Выберите Вариант", reply_markup=markup)
+
 
     bot.infinity_polling()
     bot.polling()
