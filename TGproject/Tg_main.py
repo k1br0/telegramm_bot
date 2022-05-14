923263ab64bf808b9c4bd34c151e932132d7b386
k1br0 <81685132+k1br0@users.noreply.github.com>
2022-05-15 01:53:32 +0300

    Update Tg_main.py
    Добавил кнопки в вывод на их отклик

--- TGproject/Tg_main.py
+++ TGproject/Tg_main.py
@@ -7,23 +7,27 @@ def telegram_bot(token):
     bot = telebot.TeleBot(token)
     @bot.message_handler(commands=["start"])
     def start(message):
-        bot.send_message(message.chat.id ,
-                         f"<b>Добрый день, {message.from_user.first_name}</b>" ,
-                     parse_mode="html")
-        bot.send_message(message.chat.id, f"Это телеграм бот, где ты можешь следить за миром криптовылют\nВыбери что конкретно тебя интересует...",
-                         parse_mode="html")
-
-    @bot.message_handler(commands=["start"])
-    def webite(message):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         curse = types.KeyboardButton("Курсы валют")
         news = types.KeyboardButton("Новости крипто мира")
-        markup.add(curse , news)
-        bot.send_message(message.chat.id , "Выберите Вариант", reply_markup=markup)
+        markup.add(curse, news)
 
+        bot.send_message(message.chat.id ,
+                         f"<b>Добрый день, {message.from_user.first_name}</b>",parse_mode="html")
+        bot.send_message(message.chat.id,
+                         f"Это телеграм бот, где ты можешь следить за миром криптовалют\nВыбери что конкретно тебя интересует...", reply_markup=markup)
+    bot.polling(none_stop=True)
 
-    bot.infinity_polling()
-    bot.polling()
+    @bot.message_handler(content_types = ["text"])
+    def curse(message):
+        if massage.text == "Курсы валют":
+            try:
+                bot.send_message(message.chat.id, "Выберите какая пара валют вас интересует?")
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Что-то пошло не так")
+        else:
+            bot.send_message(message.chat.id, "Я пока вас не понимаю")
 
 
 if __name__ == '__main__':
