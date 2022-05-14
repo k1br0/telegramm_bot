6846bc49487b964d39c64f781c0d78b9bae930d7
k1br0 <81685132+k1br0@users.noreply.github.com>
2022-05-15 02:13:13 +0300

    Решил проблему с выводом текста

--- TGproject/Tg_main.py
+++ TGproject/Tg_main.py
@@ -5,30 +5,33 @@ from auth_data import token
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
+
     @bot.message_handler(commands=["start"])
-    def start(message):
+    def start(start_message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         curse = types.KeyboardButton("Курсы валют")
         news = types.KeyboardButton("Новости крипто мира")
         markup.add(curse, news)
 
-        bot.send_message(message.chat.id ,
-                         f"<b>Добрый день, {message.from_user.first_name}</b>",parse_mode="html")
-        bot.send_message(message.chat.id,
+        bot.send_message(start_message.chat.id ,
+                         f"<b>Добрый день, {start_message.from_user.first_name}</b>", parse_mode="html")
+        bot.send_message(start_message.chat.id,
                          f"Это телеграм бот, где ты можешь следить за миром криптовалют\nВыбери что конкретно тебя интересует...", reply_markup=markup)
-    bot.polling(none_stop=True)
+
 
     @bot.message_handler(content_types = ["text"])
-    def curse(message):
-        if massage.text == "Курсы валют":
+    def curse(curs_news_message):
+        if curs_news_message.text == "Курсы валют":
             try:
-                bot.send_message(message.chat.id, "Выберите какая пара валют вас интересует?")
+                bot.send_message(curs_news_message.chat.id, "Выберите какая пара валют вас интересует?")
             except Exception as ex:
                 print(ex)
-                bot.send_message(message.chat.id, "Что-то пошло не так")
+                bot.send_message(curs_news_message.chat.id, "Что-то пошло не так")
         else:
-            bot.send_message(message.chat.id, "Я пока вас не понимаю")
+            bot.send_message(curs_news_message.chat.id, "Я пока вас не понимаю")
 
+
+    bot.polling(none_stop=True)
 
 if __name__ == '__main__':
     telegram_bot(token)
